0053aae700445e11b81da1120a26ded2886548e2
Dominik Gresch <greschd@gmx.ch>
2017-07-18 21:37:32 +0200

    Move generate_projections into public function

--- aiida_wannier90/calculations/wannier90.py
+++ aiida_wannier90/calculations/wannier90.py
@@ -23,6 +23,8 @@ try:
 except ImportError:
     from aiida.execmanager import get_authinfo
 
+from ..orbitals import generate_projections as _generate_projections 
+
 __authors__ = "Daniel Marchand, Antimo Marrazzo, Dominik Gresch & The AiiDA team."
 __copyright__ = u"Copyright (c), This file is part of the AiiDA platform. For further information please visit http://www.aiida.net/. All rights reserved"
 __license__ = "Non-Commercial, End-User Software License Agreement, see LICENSE.txt file."
@@ -71,7 +73,7 @@ class Wannier90Calculation(JobCalculation):
     #    self._PREFIX = 'aiida'
     #    self._PREFIX_GW = 'aiida.gw'
         self._SEEDNAME = 'aiida'
-        self._default_parser = 'wannier90'
+        self._default_parser = 'wannier90.wannier90'
         #self._INPUT_SUBFOLDER = "./out/"
         self._ALWAYS_SYM_FILES = ['UNK*', '*.mmn']
         self._RESTART_SYM_FILES = ['*.amn','*.eig']
@@ -538,7 +540,7 @@ class Wannier90Calculation(JobCalculation):
             remote_folder_content = t_dest.listdir(path=remote_input_folder_path)
 
         def files_finder(file_list, exact_patterns, glob_patterns):
-            result = [f in exact_patterns if f in file_list]
+            result = [f for f in exact_patterns if (f in file_list)]
             import fnmatch
             for glob_p in glob_patterns:
                 result += fnmatch.filter(file_list, glob_patterns)
@@ -547,7 +549,10 @@ class Wannier90Calculation(JobCalculation):
         found_in_local = files_finder(local_folder_content, input_files, wavefunctions_files)
         found_in_remote = files_finder(remote_folder_content, input_files, wavefunctions_files)
         found_in_remote = [f for f in found_in_remote if f not in found_in_local]
-        not_found = [f in required_files if f not in found_in_remote + found_in_local]
+        not_found = [
+            f for f in required_files
+            if f not in found_in_remote + found_in_local
+        ]
         if not len(not_found)!=0:
             raise InputValidationError("{} necessary input files were not found: {} "
                                        .format(len(not_found),''.join(str(nf) for nf in not_found)))
@@ -673,256 +678,11 @@ class Wannier90Calculation(JobCalculation):
 
         return calcinfo
 
-    def _gen_wannier_orbitals(cls, position_cart=None, structure=None,
-                             kind_name=None, radial=1,
-                             ang_mtm_name=None, ang_mtm_l=None,
-                             ang_mtm_mr=None, spin=None,
-                             zona=None, zaxis=None,
-                             xaxis=None, spin_axis=None):
-        """
-        Use this method to emulate the input style of wannier90,
-        when setting the orbitals (see chapter 3 in the user_guide). Position
-        can be provided either in cartesian coordiantes using position_cart
-        or can be assigned based on an input structure and kind_name.
-
-        :param position_cart: position in cartesian coordinates or list of
-                              positions in cartesian coodriantes
-        :param structure: input structure for use with kind_names
-        :param kind_name: kind_name, for use with the structure
-        :param radial: number of radial nodes
-        :param ang_mtm_name: orbital name or list of orbital names, cannot
-                             be used in conjunction with ang_mtm_l or
-                             ang_mtm_mr
-        :param ang_mtm_l: angular momentum, if ang_mtm_mr is not specified
-                          will return all orbitals associated with it
-        :param ang_mtm_mr: magnetic angular momentum number must be specified
-                           along with ang_mtm_l
-        :param spin: the spin, spin up can be specified with 1,u or U and
-                     spin down can be specified using -1,d,D
-        :param zona: as specified in user guide, applied to all orbitals
-        :param zaxis: the zaxis, list of three floats
-                      as described in wannier user guide
-        :param xaxis: the xaxis, list of three floats as described in the
-                      wannier user guide
-        :param spin_axis: the spin alignment axis, as described in the
-                          user guide
-        """
-        def convert_to_list(item):
-            """
-            internal method, checks if the item is already a list or tuple.
-            if not returns a tuple containing only item, otherwise returns
-            tuple(item)
-            """
-            if isinstance(item,(list,tuple)):
-                return tuple(item)
-            else:
-                return tuple([item])
-
-        def combine_dictlists(dict_list1, dict_list2):
-            """
-            Creates a list of every dict in dict_list1 updated with every
-            dict in dict_list2
-            """
-            out_list =  [ ]
-            # excpetion handling for the case of empty dicts
-            dict_list1_empty = not any([bool(x) for x in dict_list1])
-            dict_list2_empty = not any([bool(x) for x in dict_list2])
-            if dict_list1_empty and dict_list2_empty:
-                raise InputValidationError('One dict must not be empty')
-            if dict_list1_empty:
-                return dict_list2
-            if dict_list2_empty:
-                return dict_list2
-
-            for dict_1 in dict_list1:
-                for dict_2 in dict_list2:
-                    temp_1 = dict_1.copy()
-                    temp_2 = dict_2.copy()
-                    temp_1.update(temp_2)
-                    out_list.append(temp_1)
-            return out_list
-
-        RealhydrogenOrbital = OrbitalFactory('realhydrogen')
-
-        #########################################################################
-        # Validation of inputs                                                  #
-        #########################################################################
-        if position_cart == None and kind_name == None:
-            raise InputValidationError('Must supply a kind_name or position')
-        if position_cart != None and kind_name != None:
-            raise InputValidationError('Must supply position or kind_name'
-                                       ' not both')
-
-        structure_class = DataFactory('structure')
-        if kind_name != None:
-            if not isinstance(structure, structure_class):
-                raise InputValidationError('Must supply a StructureData as '
-                                            'structure if using kind_name')
-            if not isinstance(kind_name, basestring):
-                raise InputValidationError('kind_name must be a string')
-
-        if ang_mtm_name == None and ang_mtm_l == None:
-            raise InputValidationError("Must supply ang_mtm_name or ang_mtm_l")
-        if ang_mtm_name != None and (ang_mtm_l != None or ang_mtm_mr != None):
-            raise InputValidationError("Cannot supply ang_mtm_l or ang_mtm_mr"
-                                       " but not both")
-        if ang_mtm_l == None and ang_mtm_mr != None:
-            raise InputValidationError("Cannot supply ang_mtm_mr without "
-                                       "ang_mtm_l")
-
-        ####################################################################
-        #Setting up initial basic parameters
-        ####################################################################
-        projection_dict = {}
-        if radial:
-            projection_dict['radial_nodes'] = radial-1
-        if xaxis:
-            projection_dict['x_orientation'] = xaxis
-        if zaxis:
-            projection_dict['z_orientation'] = zaxis
-        if kind_name:
-            projection_dict['kind_name'] = kind_name
-        if spin_axis:
-            projection_dict['spin_orientation'] = spin_axis
-        if zona:
-            projection_dict['diffusivity'] = zona
-
-        projection_dicts = [projection_dict]
-
-        #####################################################################
-        # Setting up Positions                                              #
-        #####################################################################
-        # finds all the positions to append the orbitals to (if applicable)
-        position_list = []
-        if kind_name:
-            for site in structure.sites:
-                if site.kind_name == kind_name:
-                    position_list.append(site.position)
-            if len(position_list) == 0:
-                raise InputValidationError("No valid positions found in structure "
-                                        "using {}".format(kind_name))
-        # otherwise turns position into position_list
-        else:
-            position_list = [convert_to_list(position_cart)]
-        position_dicts = [{"position":v} for v in position_list]
-        projection_dicts = combine_dictlists(projection_dicts, position_dicts)
-
-        #######################################################################
-        # Setting up angular momentum                                         #
-        #######################################################################
-        # if ang_mtm_l, ang_mtm_mr provided, setup dicts
-        if ang_mtm_l is not None:
-            ang_mtm_l = convert_to_list(ang_mtm_l)
-            ang_mtm_dicts = []
-            for l in ang_mtm_l:
-                if l >= 0:
-                    ang_mtm_dicts += [{'angular_momentum':l,'magnetic_number':i}
-                                      for i in range(2*l+1)]
-                else:
-                    ang_mtm_dicts += [{'angular_momentum':l,'magnetic_number':i}
-                                      for i in range(-l+1)]
-            if ang_mtm_mr is not None:
-                if len(ang_mtm_l) > 1:
-                    raise InputValidationError("If you are giving specific"
-                                               " magnetic numbers please do"
-                                               " not supply more than one"
-                                               " angular number.")
-                ang_mtm_mr = convert_to_list(ang_mtm_mr)
-                ang_mtm_l_num = ang_mtm_l[0]
-                ang_mtm_dicts = [{'angular_momentum':ang_mtm_l_num,
-                                  'magnetic_number':j-1} for j in ang_mtm_mr]
-        if ang_mtm_name is not None:
-            ang_mtm_names =  convert_to_list(ang_mtm_name)
-            ang_mtm_dicts = []
-            for name in ang_mtm_names:
-                ang_mtm_dicts += RealhydrogenOrbital.get_quantum_numbers_from_name(name)
-        projection_dicts = combine_dictlists(projection_dicts, ang_mtm_dicts)
-
-        #####################################################################
-        # Setting up the spin                                               #
-        #####################################################################
-        if spin:
-            spin_dict = {'U':1,'u':1,1:1,'D':-1,'d':-1,-1:-1}
-            if isinstance(spin, (list,tuple)):
-                spin = [spin_dict[x] for x in spin]
-            else:
-                spin = [spin_dict[spin]]
-            spin_dicts = [{'spin':v} for v in spin]
-            projection_dicts = combine_dictlists(projection_dicts, spin_dicts)
-
-        # generating and returning a list of all corresponding orbitals
-        orbital_out = []
-        for projection_dict in projection_dicts:
-            realh = RealhydrogenOrbital()
-            realh.set_orbital_dict(projection_dict)
-            orbital_out.append(realh)
-        return orbital_out
-
-
-    def gen_projections(self, list_of_projection_dicts):
-        """
-        Use this method to emulate the input style of wannier90,
-        when setting the orbitals (see chapter 3 in the wannier90 user guide).
-        Position can be provided either in cartesian coordiantes using
-        position_cart or can be assigned based on an input structure and
-        kind_name. Pass a **list of dictionaries**, in which the keys of each
-        dictionary correspond to those below. Also that *radial*,
-        and *ang_mtm_mr* both use 0 indexing as opposed to 1 indexing,
-        effectively meaning that both should be offset by 1. E.g. an orbital
-        with 1 radial node would use radial=2 (wannier90 syntax), and then
-        be converted to radial_nodes=1 (AiiDa plugin syntax)
-        inside the stored orbital.
-
-        .. note:: The key entries used here, may not correspond to the keys used
-                  internally by the orbital objects, for example, ``ang_mtm_mr``
-                  will be converted to ``magnetic_number`` in the orbital object
-                  the value stored in orbital is listed in (braces).
-
-        .. note:: To keep in line with python-indexing as much as possible,
-                  the values of radial, and ang_mtm_mr our out of sync with
-                  their radial_nodes, angular_momentum counterparts.
-                  Specifically, radial and ang_mtm_mr both start at 1 while
-                  radial_nodes and angular_momentum both start at 0, thus
-                  making the two off by a factor of 1.
-
-        :param position_cart: position in cartesian coordinates or list of
-                              positions in cartesian coordinates (position)
-        :param kind_name: kind_name, for use with the structure (kind_name)
-        :param radial: number of radial nodes (radial_nodes + 1)
-        :param ang_mtm_name: orbital name or list of orbital names, cannot
-                             be used in conjunction with ang_mtm_l or
-                             ang_mtm_mr (See ang_mtm_l and ang_mtm_mr)
-        :param ang_mtm_l: angular momentum, if ang_mtm_mr is not specified
-                          will return all orbitals associated with it
-                          (angular_momentum)
-        :param ang_mtm_mr: magnetic angular momentum number must be specified
-                           along with ang_mtm_l (magnetic_number + 1)
-        :param spin: the spin, spin up can be specified with 1,u or U and
-                     spin down can be specified using -1,d,D (spin)
-        :param zona: as specified in user guide, applied to all orbitals
-                     (diffusivity)
-        :param zaxis: the zaxis, list of three floats
-                      as described in wannier user guide (z_orientation)
-        :param xaxis: the xaxis, list of three floats as described in the
-                      wannier user guide (x_orientation)
-        :param spin_axis: the spin alignment axis, as described in the
-                          user guide (spin_orientation)
-        """
-        try:
-            structure = self.get_inputs_dict()['structure']
-        except NameError:
-            raise InputValidationError("Must set structure first")
-        if not isinstance(list_of_projection_dicts,(list,tuple)):
-            list_of_projection_dicts = [list_of_projection_dicts]
-        orbitals = []
-        for this_dict in list_of_projection_dicts:
-            if 'kind_name' in this_dict:
-                this_dict.update({'structure':structure})
-            orbitals += self._gen_wannier_orbitals(**this_dict)
-        orbitaldata = DataFactory('orbital')()
-        orbitaldata.set_orbitals(orbitals)
-        return orbitaldata
-
+    def generate_projections(self, list_of_projection_dicts):
+        return _generate_projections(
+            list_of_projection_dicts,
+            self.get_inputs_dict()['structure']
+        )
 
 def _print_wann_line_from_orbital(orbital):
     """
--- tests/test_local_input.py
+++ tests/test_local_input.py
@@ -2,9 +2,17 @@
 # -*- coding: utf-8 -*-
 
 import os
+import pymatgen
 
-def test_local_input(sample, configure_with_daemon):
+def test_local_input(sample, configure, get_process_inputs):
     from aiida.orm import DataFactory, CalculationFactory
+    from aiida_wannier90.orbitals import generate_projections
+
+    process, inputs = get_process_inputs(
+        calculation_string='wannier90.wannier90',
+        code_string='wannier90'
+    )
+
     FolderData = DataFactory('folder')
     local_input_folder = FolderData()
     sample_folder = sample('gaas')
@@ -14,3 +22,37 @@ def test_local_input(sample, configure_with_daemon):
             continue
         abs_path = os.path.join(sample_folder, path)
         local_input_folder.add_path(abs_path, path.replace('gaas', 'aiida'))
+    inputs.local_input_folder = local_input_folder
+
+    a = 5.367 * pymatgen.core.units.bohr_to_ang
+    structure_pmg = pymatgen.Structure(
+        lattice=[[-a, 0, a], [0, a, a], [-a, a, 0]],
+        species=['Ga', 'As'],
+        coords=[[0] * 3, [0.25] * 3]
+    )
+    structure = DataFactory('structure')()
+    structure.set_pymatgen_structure(structure_pmg)
+    inputs.structure = structure
+
+    inputs.projections = generate_projections(
+        {'kind_name': 'As', 'ang_mtm_name': 'sp3'},
+        structure=structure
+    )
+
+    KpointsData = DataFactory('array.kpoints')
+    kpoints = KpointsData()
+    kpoints.set_kpoints_mesh([2, 2, 2])
+    inputs.kpoints = KpointsData()
+
+    kpoints_path = KpointsData()
+    kpoints_path.set_cell_from_structure(structure)
+    kpoints_path.set_kpoints_path()
+    inputs.kpoints_path = kpoints_path
+
+    inputs.parameters = DataFactory('parameter')(dict=dict(
+        num_wann=4,
+        num_iter=12,
+        wvfn_formatted=True
+    ))
+
+    process.run(**inputs)
