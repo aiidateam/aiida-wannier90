7a571867373a3d3cc5840d2368ac896a24afa88f
Dominik Gresch <greschd@gmx.ch>
2017-07-21 15:55:42 +0200

    Add test with duplicate entry for exclude_bands

--- tests/test_local_input.py
+++ tests/test_local_input.py
@@ -7,7 +7,7 @@ import pymatgen
 
 @pytest.fixture
 def create_gaas_calc(get_process_inputs, sample, configure):
-    def inner():
+    def inner(projections_dict={'kind_name': 'As', 'ang_mtm_name': 'sp3'}):
         from aiida.orm import DataFactory, CalculationFactory
         from aiida_wannier90.orbitals import generate_projections
 
@@ -38,7 +38,7 @@ def create_gaas_calc(get_process_inputs, sample, configure):
         inputs.structure = structure
 
         inputs.projections = generate_projections(
-            {'kind_name': 'As', 'ang_mtm_name': 'sp3'},
+            projections_dict,
             structure=structure
         )
 
@@ -63,5 +63,20 @@ def create_gaas_calc(get_process_inputs, sample, configure):
 def test_local_input(create_gaas_calc, configure_with_daemon):
     from aiida.work.run import run
     process, inputs = create_gaas_calc()
+    output = run(process, **inputs)
+    assert all(key in output for key in ['retrieved', 'output_parameters'])
+
+def test_duplicate_exclude_bands(create_gaas_calc, configure_with_daemon):
+    from aiida.work.run import run
+    from aiida.orm import DataFactory
+    process, inputs = create_gaas_calc(
+        projections_dict={'kind_name': 'As', 'ang_mtm_name': 's'}
+    )
+    inputs.parameters = DataFactory('parameter')(dict=dict(
+        num_wann=1,
+        num_iter=12,
+        wvfn_formatted=True,
+        exclude_bands=[1] * 2 + [2, 3]
+    ))
     output = run(process, **inputs)
     assert all(key in output for key in ['retrieved', 'output_parameters'])
