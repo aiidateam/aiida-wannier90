14d3dba8b12f92a0f354e6a4bfc6f869e0fa329a
Dominik Gresch <greschd@gmx.ch>
2017-11-14 14:26:42 +0100

    Fix caching test

---
+++ tests/test_caching.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+import pytest
+
+from gaas_sample import *
+
+def test_caching(create_gaas_calc, configure_with_daemon, assert_finished):
+    from aiida.work.run import run
+    from aiida.orm import load_node
+    from aiida.common.hashing import make_hash
+    process, inputs = create_gaas_calc()
+    output, pid = run(process, _return_pid=True, **inputs)
+    output2, pid2 = run(process, _use_cache=True, _return_pid=True, **inputs)
+    assert 'cached_from' in load_node(pid2).extras()
+    assert all(key in output for key in ['retrieved', 'output_parameters'])
+    assert all(key in output2 for key in ['retrieved', 'output_parameters'])
+    assert_finished(pid)
+    assert_finished(pid2)
