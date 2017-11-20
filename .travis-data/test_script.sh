b854f9814a073462454228bcb38bc62913d8e24e
Dominik Gresch <greschd@gmx.ch>
2017-11-20 11:24:59 +0100

    Set quiet-wipe flag for aiida-pytest

--- .travis-data/test_script.sh
+++ .travis-data/test_script.sh
@@ -9,7 +9,7 @@ case "$TEST_TYPE" in
         cd ${TRAVIS_BUILD_DIR}/.travis-data; ./build_wannier90.sh
         python ${TRAVIS_BUILD_DIR}/.travis-data/configure.py ${TRAVIS_BUILD_DIR}/.travis-data ${TRAVIS_BUILD_DIR}/tests;
         export AIIDA_PATH="${TRAVIS_BUILD_DIR}/tests"
-        cd ${TRAVIS_BUILD_DIR}/tests; py.test
+        cd ${TRAVIS_BUILD_DIR}/tests; py.test --quiet-wipe
         ;;
     pre-commit)
         pre-commit run --all-files || git status --short && git diff
