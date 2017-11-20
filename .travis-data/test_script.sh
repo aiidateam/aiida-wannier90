0c71cc59025c848d9cb08f6395fdfd0bb632c4bb
Dominik Gresch <greschd@gmx.ch>
2017-11-20 11:17:14 +0100

    Set AIIDA_PATH

--- .travis-data/test_script.sh
+++ .travis-data/test_script.sh
@@ -8,6 +8,7 @@ case "$TEST_TYPE" in
         # Run the AiiDA tests
         cd ${TRAVIS_BUILD_DIR}/.travis-data; ./build_wannier90.sh
         python ${TRAVIS_BUILD_DIR}/.travis-data/configure.py ${TRAVIS_BUILD_DIR}/.travis-data ${TRAVIS_BUILD_DIR}/tests;
+        export AIIDA_PATH="${TRAVIS_BUILD_DIR}/tests"
         cd ${TRAVIS_BUILD_DIR}/tests; py.test
         ;;
     pre-commit)
