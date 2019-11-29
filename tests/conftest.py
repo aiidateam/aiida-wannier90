ee8f729e8d59841be86f43de6d3db1b6063f2b2c
Dominik Gresch <a-dogres@microsoft.com>
2019-11-29 15:17:51 +0100

    Remove commented out 'generate_remote_data'.
    This function was copied directly from aiida-qe. We probably want
    something similar, but this can be put directly in the test for
    remote input.

--- tests/conftest.py
+++ tests/conftest.py
@@ -279,37 +279,3 @@ def generate_kpoints_mesh():
         return kpoints
 
     return _generate_kpoints_mesh
-
-
-# @pytest.fixture
-# def generate_remote_data():
-#     """Return a `RemoteData` node."""
-#     def _generate_remote_data(computer, remote_path, entry_point_name=None):
-#         """Return a `KpointsData` with a mesh of npoints in each direction."""
-#         from aiida.common.links import LinkType
-#         from aiida.orm import CalcJobNode, RemoteData
-#         from aiida.plugins.entry_point import format_entry_point_string
-
-#         entry_point = format_entry_point_string(
-#             'aiida.calculations', entry_point_name
-#         )
-
-#         remote = RemoteData(remote_path=remote_path)
-#         remote.computer = computer
-
-#         if entry_point_name is not None:
-#             creator = CalcJobNode(computer=computer, process_type=entry_point)
-#             creator.set_option(
-#                 'resources', {
-#                     'num_machines': 1,
-#                     'num_mpiprocs_per_machine': 1
-#                 }
-#             )
-#             remote.add_incoming(
-#                 creator, link_type=LinkType.CREATE, link_label='remote_folder'
-#             )
-#             creator.store()
-
-#         return remote
-
-#     return _generate_remote_data
