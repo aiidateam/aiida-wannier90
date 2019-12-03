b82ee6363d00e0d92e4fdd605a87a9fc07ea576a
Dominik Gresch <a-dogres@microsoft.com>
2019-12-03 10:30:22 +0100

    Add test for parsing when 'kpoint_path' is not set. Remove catching 'AttributeError', since this is the case which now triggers the 'NotExistent' exception.

--- tests/parsers/test_parser.py
+++ tests/parsers/test_parser.py
@@ -36,3 +36,35 @@ def test_wannier_default(
         'output_parameters':
         results['output_parameters'].get_dict(),
     })
+
+
+def test_no_kpoint_path(
+    fixture_localhost,
+    generate_calc_job_node,
+    generate_parser,
+    generate_win_params_gaas,
+    data_regression,
+):
+    """Check that parsing still works if the 'kpoint_path' is not set."""
+    inputs = generate_win_params_gaas()
+    del inputs['kpoint_path']
+    node = generate_calc_job_node(
+        entry_point_name=ENTRY_POINT_CALC_JOB,
+        computer=fixture_localhost,
+        test_name='gaas/seedname_aiida',
+        inputs=inputs,
+    )
+    parser = generate_parser(ENTRY_POINT_PARSER)
+    results, calcfunction = parser.parse_from_node(
+        node, store_provenance=False
+    )
+
+    assert calcfunction.is_finished, calcfunction.exception
+    assert calcfunction.is_finished_ok, calcfunction.exit_message
+    assert not orm.Log.objects.get_logs_for(node)
+    assert 'output_parameters' in results
+
+    data_regression.check({
+        'output_parameters':
+        results['output_parameters'].get_dict(),
+    })
