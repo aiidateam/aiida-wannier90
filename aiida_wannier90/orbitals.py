8a49b11d380ea98907ecb5699340ba9517293724
Dominik Gresch <a-dogres@microsoft.com>
2020-03-06 18:40:23 +0100

    Remove AiiDA 1.0 compatibility in documentation conf.py
    Also remove unnecessary shebangs in module files -- these can
    not be executed directly.

--- aiida_wannier90/orbitals.py
+++ aiida_wannier90/orbitals.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 ################################################################################
 # Copyright (c), AiiDA team and individual contributors.                       #
